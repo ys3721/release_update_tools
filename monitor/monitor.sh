106c4b1baeeb52ced6d4f024fdf1a3b2fba3b2b7
Yao Shuai <ys3721@hotmail.com>
2019-05-14 19:03:47 +0800

    monitor监控服务器挂了

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -9,3 +9,5 @@ if [ $server_count -ne $process_count ]; then
     python sms.py 15811302052 "紧急【三群-玩吧-h5 t1 or t2】服务器异常，服务器已经异常停止！请检查！"
     python sms.py 13212777262 "紧急【三群-玩吧-h5】服务器异常，服务器已经异常停止！请检查！"
 fi
+
+df |grep /dev/ | awk '{print $5}'
