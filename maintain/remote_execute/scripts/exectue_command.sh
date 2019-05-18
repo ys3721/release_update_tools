72017b67ba5e8f1edbbf5210db33d678a4fb1ad2
Shuai Yao <ys3721@hotmail.com>
2019-05-18 22:43:24 +0800

    要执行下 看看服务器的内存还有多少

--- maintain/remote_execute/scripts/exectue_command.sh
+++ maintain/remote_execute/scripts/exectue_command.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
 target_server=$1
-command=$2
 if [ -n $target_server ]; then
     echo "target_server is null!"
-    return 1
-fi
-
-if [ -n $comnand ]; then
-    echo "command is null!"
-    return 1
+    exit 1
 fi
 
 file=/data2/servers/${target_server}.config
 if [ ! -f $file ]; then
     echo "$file is not exist!!"
-    return 1
+    exit 1
 fi
 
 
-lan_ip=`cat $file | ask '{print $4}'`
+lan_ip=`cat $file | awk '{print $4}'`
 
 
-ssh root@$lan_ip $command
+ssh root@$lan_ip df
