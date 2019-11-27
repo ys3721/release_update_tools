46f092a613bfe3bc198e87b72620e4487901a1d5
Yao Shuai <ys3721@hotmail.com>
2019-11-27 15:37:28 +0800

    考呗数据到指定的服务器代码的提交

--- maintain/move/copy_sql_to_loader.py
+++ maintain/move/copy_sql_to_loader.py
@@ -9,28 +9,31 @@ import re
 argv = sys.argv
 to_ip = ''
 from_name = ''
+from_port = ''
 try:
-    opts, args = getopt.getopt(argv[1:], "ht:n:",["help","targetIp=","serverName="])
+    opts, args = getopt.getopt(argv[1:], "ht:n:p:",["help","targetIp=","serverName=","port="])
 except getopt.GetoptError:
-    print argv[0]+" -t <targetIp>"
+    print argv[0]+" -n <this server name> -t <targetIp> -p <port>"
     sys.exit(2)
 for opt_name, opt_value in opts:
     print opt_name, opt_value
     if opt_name in ('-h','help'):
-        print 1
+        print argv[0] + " -n <this server name> -t <targetIp> -p <port>"
+        exit(1)
     if opt_name in ('-t', 'targetIp'):
         to_ip = opt_value
     if opt_name in ('-n', 'serverName'):
         from_name = opt_value
-
+    if opt_name in ('-p', 'port'):
+        from_port = opt_value
 
 data_pattern = re.compile(r"""^(.*?)((19|20)\d\d*)(.*?)$""", re.VERBOSE)
 newest_file_data = 0
 
+need_search_files_pattern = "\w*_%s.sql" % str(from_port)
 for folderName, subfolders, filenames in os.walk('/data0/sql_bak/'):
     for file_name in filenames:
-
-        if re.match("\w*_3306.sql",file_name):
+        if re.match(need_search_files_pattern, file_name):
             mo = data_pattern.search(file_name)
             if mo == None:
                 continue
@@ -38,7 +41,7 @@ for folderName, subfolders, filenames in os.walk('/data0/sql_bak/'):
             if data_stamp > newest_file_data:
                 newest_file_data = data_stamp
 
-newest_file_name = "/data0/sql_bak/" + str(newest_file_data) + "_3306.sql"
+newest_file_name = "/data0/sql_bak/" + str(newest_file_data) + "_"+str(from_port)+".sql"
 print "I will copy newest file to server, file=" + newest_file_name
-os.system("echo scp %s root@%s:/data0/src/%s" % (newest_file_name, to_ip, from_name+"_"+str(newest_file_data))+"_3306.sql")
-os.system("scp %s root@%s:/data0/src/%s" % (newest_file_name, to_ip, from_name+"_"+str(newest_file_data))+"_3306.sql")
+os.system("echo scp %s root@%s:/data0/src/%s" % (newest_file_name, to_ip, from_name+"_"+str(newest_file_data))+"_"+str(from_port)+".sql")
+#os.system("scp %s root@%s:/data0/src/%s" % (newest_file_name, to_ip, from_name+"_"+str(newest_file_data))+"_"+str(from_port)+".sql")
