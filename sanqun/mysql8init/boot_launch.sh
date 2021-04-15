1eddd01e78cb1c4a89a14e00d682f561c95bf60a
Yao Shuai <ys3721@hotmail.com>
2021-04-15 20:48:28 +0800

    终风且暴，顾我则笑。谑浪笑敖，中心是悼。

--- sanqun/mysql8init/boot_launch.sh
+++ sanqun/mysql8init/boot_launch.sh
@@ -7,23 +7,26 @@
  * *******************************************************
 !
 
-function foo() {
+function start() {
   export JAVA_HOME=/usr/local/jdk8/
+  if [ ! -d ./logs ] ; then
+    mkdir "./logs/"
+  fi
 
-  jarfile=${ls ./*.jar}
+  jarfile=$(ls *jar)
   java -Dapp.name=nppa-server -server -Xmx2048m -Xms2048m  -XX:NewRatio=2 -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 \
-      -XX:ParallelGCThreads=8 -XX:ConcGCThreads=6 -XX:G1ReservePercent=10 -XX:InitiatingHeapOccpancyPercent=70 \
-      -XX:+PrintGCDetails -XX:+UseG1GC -jar $jarfile >>./logs/$jarfile.std.log 2>$1 &
+      -XX:ParallelGCThreads=8 -XX:ConcGCThreads=6 -XX:G1ReservePercent=10 -XX:InitiatingHeapOccupancyPercent=70 \
+      -XX:+PrintGCDetails -Xloggc:./logs/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./logs/dumpcrash.bin -XX:+UseG1GC -jar $jarfile >> ./logs/boot.log 2>&1 &
   pid=$!
-  echo "$pid" > ./${jarfile}.pid
-  echo "Sprintboot ./${jarfile} Process Id:$pid"
+  echo "$pid" > ./boot.pid
+  echo "Sprintboot ${jarfile} Process Id:$pid"
 }
 
 function stop(){
-        pid=`cat ./${jarfile}.pid`
+        pid=`cat ./boot.pid`
         echo "Stop Spring Boot Process Id:$pid"
         kill $pid
-        rm -f ./${jarfile}.pid
+        rm -f ./boot.pid
 }
 
 case "$1" in
