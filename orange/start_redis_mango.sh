e77cbc942280300956d3769a2f8f38c1499dd56a
yaoshuai <yaoshuai@hortorgames.com>
2021-07-10 17:13:00 +0800

    start scritp

---
+++ orange/start_redis_mango.sh
@@ -0,0 +1,9 @@
+#!/usr/bin bash
+<<!
+ **********************************************************
+ * Author        : Yao Shuai
+ * 本地启动一些依赖的. 数据库  redis
+ * *******************************************************
+!
+echo 'hortor' | sudo nohup mongod --dbpath ~/data0/mongodb 1>&2 1>/dev/null &
+echo 'hortor' | sudo redis-server /etc/redis/redis.conf
