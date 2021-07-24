80574b35e4ede801a566715c7e7e6618253e6e8f
yaoshuai <yaoshuai@hortorgames.com>
2021-07-24 14:14:14 +0800

    [Script]整理一下结构吧，把修复一些问题的shell集中到一起

--- remote_execute/newbatch/new_example_batch_command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# sh new_batch_server.sh new_example_batch_command.sh h1-h50 /data2/servers true (ip去重)
-# sh new_batch_server.sh new_example_batch_command.sh h1-h50 /data2/servers false (ip不去重复)
-ip="$1"
-echo "执行中"$ip
-ssh $ip "ls";
--- remote_execute/newbatch/scripts/del/startmysql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ip="$1"
-ssh $ip "export PATH=$PATH:/usr/local/mysql/bin/;sh /etc/init.d/mysqld_multi.server start";
