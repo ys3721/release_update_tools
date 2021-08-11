d237d92b767a95659779e76b2a2106f1a9de6738
yaoshuai <ys3721@hotmail.com>
2021-08-12 00:18:47 +0800

    [script]start init

--- orange/start_redis_mango.sh
+++ orange/start_redis_mango.sh
@@ -5,5 +5,9 @@
  * 本地启动一些依赖的. 数据库  redis
  * *******************************************************
 !
-echo 'hortor' | sudo nohup mongod --dbpath ~/data0/mongodb 1>&2 1>/dev/null &
-echo 'hortor' | sudo redis-server /etc/redis/redis.conf
+#echo 'hortor' | sudo nohup mongod --dbpath ~/data0/mongodb 1>&2 1>/dev/null &
+#echo 'hortor' | sudo redis-server /etc/redis/redis.conf
+
+## home mac
+mongod --config /usr/local/etc/mongod.conf
+redis-server /etc/redis6379.conf
